62b46a557ab2d4073c986ae8ee137bd487b5f51f
itsmeakhiro <jasmarket577@gmail.com>
2025-02-08 05:48:38 +0800

    g

--- main.js
+++ main.js
@@ -11,5 +11,12 @@ process.on("uncaughtException", (error) => log("ERROR", error));
 
 global.Tokito = {
     get config(){
-        return JSON.parse(fs.readFileSync(path.join))
+        return JSON.parse(fs.readFileSync(path.join(__dirname, "botdata.json"), "utf-8"));
+    },
+    set config(config){
+        const data = global.Tokito.config;
+        const finalData = { ...data, ...config };
+            const str = JSON.stringify(finalData, null, 2);
+                fs.writeFileSync(path.join(__dirname, "botdata.json"), str);
     }
+}
