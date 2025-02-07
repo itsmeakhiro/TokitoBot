ba27805dea3d0b05b219cd45943cd01033bf9abe
itsmeakhiro <jasmarket577@gmail.com>
2025-02-07 19:43:45 +0800

    Update main.js

--- main.js
+++ main.js
@@ -2,3 +2,9 @@ const fs = require("fs-extra");
 const path = require("path");
 const express = require("express");
 const app = express();
+
+process.on("unhandledRejection", (...args) => log("ERROR", ...args));
+process.on("uncaughtException", (...args) => log("ERROR", ...args));
+
+process.on("unhandledRejection", (error) => log("ERROR", error));
+process.on("uncaughtException", (error) => log("ERROR", error));
