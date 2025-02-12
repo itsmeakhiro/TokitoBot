9fc761104ac6a02a999121893e5119e48f23a1c7
itsmeakhiro <jasmarket577@gmail.com>
2025-02-12 22:08:48 +0000

    update

---
+++ System/handler/eventHandler.js
@@ -0,0 +1,27 @@
+module.exports = async function eventHandler({ ...entryObj }) {
+    const { event, chat, fonts, api } = entryObj;
+  
+    try {
+      if (!event) {
+        log("ERROR", "No event data provided");
+        return;
+      }
+  
+      for (const { manifest, onEvent } of global.Tokito.events.values()) {
+        if (event && manifest.name) {
+          const args = event.body?.split(" ");
+  
+          try {
+            await onEvent({
+              ...entryObj,
+              args,
+            });
+          } catch (err) {
+            console.error(`Error handling event ${manifest.name}:`, err);
+          }
+        }
+      }
+    } catch (err) {
+      console.error("Error in eventHandler:", err);
+    }
+  };
