c6d965a4f0f20806e30e36d2854271b09930ee42
Francis Loyd Raval <jasmarket577@gmail.com>
2025-02-20 06:45:34 +0800

    Update logger.js

--- System/logger.js
+++ System/logger.js
@@ -1,3 +1,10 @@
-module.exports = async function log(){
-  
+module.exports = async function log(type, message){
+  const colors = {
+    SYSTEM: "",
+    ERROR: "\x1b[31m",
+    INFO: "",
+    WARM: "",
+    RESET: "\x1b[0m",
+  };
+  console.log(`${colors[type] || colors.SYSTEM}[ ${type} ]${colors.RESET} ${message}`)
 }
