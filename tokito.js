7678e4bcb77889a48c7927640ef353470b584041
lianecagara <nealiana.kaye.cagara@gmail.com>
2025-02-25 21:31:36 +0800

    not finished

--- tokito.js
+++ tokito.js
@@ -3,20 +3,97 @@
 const express = require("express");
 const crypto = require("crypto");
 const router = express.Router();
+const listener = require("./System/listener");
 
-global.allResolve = new Map();
+const allResolve = new Map();
 
 router.get("/postWReply", async (req, res) => {
-  const event = req.query ?? {};
-  event.senderID = `web:${event.senderID}`;
-  event.threadID = `web:${event.threadID}`;
-  event.messageID = `id_${crypto.randomUUID()}`; 
+  const event = new Event(req.query ?? {});
+  event.messageID = `id_${crypto.randomUUID()}`;
 
   const botResponse = await new Promise((resolve, reject) => {
-    global.allResolve.set(event.messageID, resolve);
+    allResolve.set(event.messageID, resolve);
   });
 
   res.json(botResponse);
 });
+
+const pref = "web:";
+
+function formatIP(ip) {
+  try {
+    ip = ip?.replaceAll("custom_", "");
+
+    const formattedIP = ip;
+
+    return `${pref}${formattedIP}`;
+  } catch (error) {
+    console.error("Error in formatting IP:", error);
+    return ip;
+  }
+}
+
+class Event {
+  constructor({ ...info } = {}) {
+    let defaults = {
+      body: "",
+      senderID: "0",
+      threadID: "0",
+      messageID: "0",
+      type: "message",
+      timestamp: Date.now().toString(),
+      isGroup: false,
+      participantIDs: [],
+      attachments: [],
+      mentions: {},
+      isWeb: true,
+    };
+    Object.assign(this, defaults, info);
+    // @ts-ignore
+    if (this.userID && this.isWeb) {
+      this.userID = formatIP(this.senderID);
+    }
+    this.senderID = formatIP(this.senderID);
+    this.threadID = formatIP(this.threadID);
+    if (
+      "messageReply" in this &&
+      typeof this.messageReply === "object" &&
+      this.messageReply
+    ) {
+      // @ts-ignore
+
+      this.messageReply.senderID = formatIP(this.messageReply.senderID);
+    }
+    if (Array.isArray(this.participantIDs)) {
+      this.participantIDs = this.participantIDs.map((id) => formatIP(id));
+    }
+
+    if (Object.keys(this.mentions ?? {}).length > 0) {
+      this.mentions = Object.fromEntries(
+        // @ts-ignore
+        Object.entries(this.mentions).map((i) => [formatIP(i[0]), i[1]])
+      );
+    }
+  }
+}
+
+const apiFake = new Proxy(
+  {},
+  {
+    get(target, prop) {
+      if (prop in target) {
+        return target[prop];
+      }
+      return (...args) => {
+        console.log(
+          `Warn: 
+api.${key}(${args
+            .map((i) => `[ ${typeof i} ${i?.constructor?.name || ""} ]`)
+            .join(",")}) has no effect!`
+        );
+      };
+    },
+  }
+);
 
 module.exports = router;
