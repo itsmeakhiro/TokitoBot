9b58e7166be5c424617e43ea3fa13ba3a091de92
Francis Loyd Raval <jasmarket577@gmail.com>
2025-02-25 12:14:30 +0000

    Nahhh....

--- tokito.js
+++ tokito.js
@@ -1,6 +1,7 @@
 // Disclaimer: This function may / has to be in beta version. So please do not intent to modify this file as this is an own developed code by Francis Loyd Raval. Do not MODIFY this if you dont want to global ban you to the website and its bot functionality. This part is where the tokito has access to become a API
 
 const express = require("express");
+const crypto = require("crypto");
 const router = express.Router();
 
 global.allResolve = new Map();
@@ -9,10 +10,12 @@ router.get("/postWReply", async (req, res) => {
   const event = req.query ?? {};
   event.senderID = `web:${event.senderID}`;
   event.threadID = `web:${event.threadID}`;
-  event.messageID = `id_${Date.now()}`;
+  event.messageID = `id_${crypto.randomUUID()}`; 
+
   const botResponse = await new Promise((resolve, reject) => {
     global.allResolve.set(event.messageID, resolve);
   });
+
   res.json(botResponse);
 });
 
