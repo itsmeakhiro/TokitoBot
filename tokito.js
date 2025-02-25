a134de9eadf08979a6e4a8589f9bd2d4ef8e7f08
Francis Loyd Raval <jasmarket577@gmail.com>
2025-02-25 13:19:52 +0800

    Update tokito.js

--- tokito.js
+++ tokito.js
@@ -1,3 +1,3 @@
-// Disclaimer: This function may / has to be in beta version. So please do not intent to modify this file as this is an own developed code by Francis Loyd Raval. Do not MODIFY this if you dont want to global ban you to the website and its bot functionality.
+// Disclaimer: This function may / has to be in beta version. So please do not intent to modify this file as this is an own developed code by Francis Loyd Raval. Do not MODIFY this if you dont want to global ban you to the website and its bot functionality. This part is where the tokito has access to become a API
 
 const log = require ("./System/logger");
