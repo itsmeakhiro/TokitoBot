7eae8be973b0c448bfdcf250268bdb64e80c4730
Francis Loyd Raval <jasmarket577@gmail.com>
2025-03-08 16:37:03 +0800

    Update utils.js

--- Tokito/resources/level/utils.js
+++ Tokito/resources/level/utils.js
@@ -5,12 +5,10 @@ const LEVEL_DATA_PATH = path.join(__dirname, "levelData.json");
 
 const RANKS = {};
 for (let i = 1; i <= 200; i++) {
-    if (i <= 5) RANKS[i] = "Untrained Recruit";
-    else if (i <= 10) RANKS[i] = "Trainee Slayer";
-    else if (i <= 15) RANKS[i] = "Demon Slayer Initiate";
-    else if (i <= 20) RANKS[i] = "Junior Slayer";
-    else if (i <= 30) RANKS[i] = "Kanoe (Lower Water)";
-    else if (i <= 40) RANKS[i] = "Kanoto (Upper Water)";
+    if (i <= 10) RANKS[i] = "New Recruit";
+    else if (i <= 20) RANKS[i] = "Trainee Slayer";
+    else if (i <= 30) RANKS[i] = "Kanoe (Upper Water)";
+    else if (i <= 40) RANKS[i] = "Kanoto (Upper Water II)";
     else if (i <= 50) RANKS[i] = "Tsuchinoto (Earth’s Base)";
     else if (i <= 60) RANKS[i] = "Tsuchinoe (Earth’s Branch)";
     else if (i <= 70) RANKS[i] = "Hinoto (Flame’s Base)";
@@ -30,121 +28,70 @@ for (let i = 1; i <= 200; i++) {
 }
 
 class LevelSystem {
-    constructor() {
-        this.levelData = this.loadData();
-        this.registeredUsers = new Set(Object.keys(this.levelData));
+    constructor(uid, username = "Unknown") {
+        this.uid = uid;
+        this.username = username;
+        this.loadData();
     }
 
     loadData() {
         if (fs.existsSync(LEVEL_DATA_PATH)) {
             try {
-                return JSON.parse(fs.readFileSync(LEVEL_DATA_PATH, "utf8"));
+                const data = JSON.parse(fs.readFileSync(LEVEL_DATA_PATH, "utf8"));
+                if (data[this.uid]) {
+                    this.xp = data[this.uid].xp;
+                    this.level = data[this.uid].level;
+                    this.rank = data[this.uid].rank;
+                    return;
+                }
             } catch (err) {
                 console.error("Error loading level data:", err);
-                return {};
             }
         }
-        return {};
+        this.xp = 0;
+        this.level = 1;
+        this.rank = RANKS[1];
+        this.saveData();
     }
 
     saveData() {
-        fs.writeFileSync(LEVEL_DATA_PATH, JSON.stringify(this.levelData, null, 2));
+        let data = {};
+        if (fs.existsSync(LEVEL_DATA_PATH)) {
+            try {
+                data = JSON.parse(fs.readFileSync(LEVEL_DATA_PATH, "utf8"));
+            } catch (err) {
+                console.error("Error reading level data:", err);
+            }
+        }
+        data[this.uid] = { xp: this.xp, level: this.level, rank: this.rank, username: this.username };
+        fs.writeFileSync(LEVEL_DATA_PATH, JSON.stringify(data, null, 2));
     }
 
-    generateGameID(uid) {
-        return `id_${Math.floor(Math.random() * 100000)}(${uid.slice(-5)})`;
-    }
-
-    registerUser(uid, username = "Unknown") {
-        if (this.registeredUsers.has(uid)) return false;
-
-        this.levelData[uid] = {
-            xp: 0,
-            level: 1,
-            username,
-            gameID: this.generateGameID(uid),
-            rank: RANKS[1]
-        };
-
-        this.registeredUsers.add(uid);
-        this.saveData();
-        return true;
-    }
-
-    isRegistered(uid) {
-        return this.registeredUsers.has(uid);
-    }
-
-    getUser(uid) {
-        return this.isRegistered(uid) ? this.levelData[uid] : null;
-    }
-
-    getLevel(uid) {
-        return this.isRegistered(uid) ? this.levelData[uid].level : null;
-    }
-
-    getRank(uid) {
-        return this.isRegistered(uid) ? this.levelData[uid].rank : null;
-    }
-
-    getEXP(uid) {
-        return this.isRegistered(uid) ? this.levelData[uid].xp : null;
-    }
-
-    getStats(uid) {
-    if (!this.isRegistered(uid)) return { error: "User not registered" };
-
-    const user = this.getUser(uid);
-    return {
-        username: user.username,
-        gameID: user.gameID,
-        xp: user.xp,
-        level: user.level,
-        rank: user.rank
-      };
-    }
-
-    addXP(uid, amount) {
-        if (!this.isRegistered(uid)) return { error: "User not registered" };
-
-        const user = this.getUser(uid);
-        user.xp += amount;
-
-        let xpNeeded = Math.floor(100 * Math.pow(1.2, user.level));
+    addXP(amount) {
+        this.xp += amount;
+        const xpNeeded = Math.floor(100 * Math.pow(1.2, this.level));
         let leveledUp = false;
 
-        while (user.xp >= xpNeeded && user.level < 200) {
-            user.xp -= xpNeeded;
-            user.level += 1;
-            xpNeeded = Math.floor(100 * Math.pow(1.2, user.level)); 
+        while (this.xp >= xpNeeded && this.level < 200) {
+            this.xp -= xpNeeded;
+            this.level += 1;
             leveledUp = true;
         }
 
-        user.rank = RANKS[user.level];
+        this.rank = RANKS[this.level];
         this.saveData();
-        return { leveledUp, newLevel: user.level, newRank: user.rank };
+        return { leveledUp, newLevel: this.level, newRank: this.rank };
     }
 
-    resetUser(uid) {
-        if (!this.isRegistered(uid)) return false;
-
-        delete this.levelData[uid];
-        this.registeredUsers.delete(uid);
-        this.saveData();
-        return true;
+    getLevel() {
+        return this.level;
     }
 
-    getLeaderboard() {
-        return Object.entries(this.levelData)
-            .sort((a, b) => b[1].level - a[1].level || b[1].xp - a[1].xp)
-            .slice(0, 5)
-            .map(([uid, data]) => ({
-                uid,
-                username: data.username,
-                gameID: data.gameID,
-                level: data.level,
-                rank: data.rank,
-                xp: data.xp
-            }));
+    getRank() {
+        return this.rank;
+    }
+
+    getXP() {
+        return this.xp;
     }
 }
