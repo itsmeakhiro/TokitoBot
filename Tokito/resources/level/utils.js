134f874ad687c2ca8751300bc3a1f40fe72c4bd6
Francis Loyd Raval <jasmarket577@gmail.com>
2025-03-18 19:48:29 +0800

    Update utils.js
    A test if it works

--- Tokito/resources/level/utils.js
+++ Tokito/resources/level/utils.js
@@ -24,35 +24,46 @@ const RANKS = [
 
 class LevelSystem {
   static RANKS = RANKS;
+
   constructor(data) {
     this.data = data ?? {};
     this.data.xp ??= 0;
   }
 
+  isRegistered() {
+    return !!this.data.username;
+  }
+
+  register(username) {
+    if (this.isRegistered()) throw new Error("User is already registered.");
+    this.data.username = username;
+  }
+
   getXP() {
+    if (!this.isRegistered()) throw new Error("User is not registered.");
     return this.data.xp;
   }
 
   addXP(exp) {
+    if (!this.isRegistered()) throw new Error("User is not registered.");
     this.data.xp += exp;
   }
 
   setXP(xp) {
+    if (!this.isRegistered()) throw new Error("User is not registered.");
     this.data.xp = Math.max(0, xp);
   }
 
   getLevel() {
+    if (!this.isRegistered()) throw new Error("User is not registered.");
     return Math.floor(0.1 * Math.sqrt(this.getXP()));
   }
 
   getRank() {
+    if (!this.isRegistered()) throw new Error("User is not registered.");
     let level = this.getLevel();
     let rank = RANKS.findLast((r) => level >= r.level);
     return rank ? rank.rank : RANKS[RANKS.length - 1]?.rank;
-  }
-
-  setUsername(username) {
-    this.data.username = username;
   }
 
   getUsername() {
